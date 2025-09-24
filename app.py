56fe160da1bcff676b0136abb70ba0126f64dd18
shicx2122 <144436242+shicx2122@users.noreply.github.com>
2025-09-24 16:53:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -128,4 +128,4 @@ def logout():
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=false, host='0.0.0.0', port=5000)
